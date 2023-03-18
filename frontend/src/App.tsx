5fb5593c38cf15bcf36fc0c8cdaf904d6127fc9b
Matvey Ryabchikov <35634442+ronanru@users.noreply.github.com>
2023-03-18 22:33:07 +0300

    new name

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
 import clsx from 'clsx';
 import { Component, createSignal } from 'solid-js';
+import { Stat } from './api';
 import Card from './components/Card';
-import ImgBtn from './components/ImgBtn';
 import Select from './components/Select';
 import { upperCaseFirstLetter } from './utils';
 
 const App: Component = () => {
   const groups = ['general', 'items', 'mobs'];
-
   const [group, setGroup] = createSignal<(typeof groups)[number]>(groups[0]);
+  const [stats, setStats] = createSignal<Stat[]>([]);
 
   return (
     <>
@@ -22,25 +22,8 @@ const App: Component = () => {
         ))}
       </section>
       <section class="space-y-2 rounded-xl bg-zinc-800 p-4">
-        <form class="items-center gap-4 space-y-4 md:flex md:space-y-0 ">
-          <input
-            id="Input"
-            type="text"
-            class="h-10 w-full rounded-lg bg-zinc-900 p-1.5 text-xl text-white"
-            placeholder="Block id"
-          />
-          {/* <select class="h-10 w-full rounded-lg bg-zinc-900 p-1.5 text-xl text-white">
-            <option>Lorem, ipsum.</option>
-            <option>Lorem, ipsum dolor.</option>
-          </select> */}
-          <Select></Select>
-        </form>
-        <div class="custom-scroll flex gap-3 overflow-x-auto text-sm">
-          <ImgBtn></ImgBtn>
-          <ImgBtn></ImgBtn>
-          <ImgBtn></ImgBtn>
-          <ImgBtn></ImgBtn>
-        </div>
+        <Select></Select>
+        <Select></Select>
       </section>
       <section class="space-y-2 rounded-xl bg-zinc-800 p-4">
         <ul class="space-y-2">
